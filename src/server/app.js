bf9b469df583aa4f9360d9cd6caeed6aad1b9199
Sébastien Castiel <sebastien.castiel@energiency.com>
2016-10-31 18:15:22 +0100

    second version – with Babel

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,9 @@
 // src/server/app.js
-var express = require('express');
+import express from 'express';
 var app = express();
 
 app.use(express.static(__dirname + '/../../public'));
+app.use('/js', express.static(__dirname + '/../../dist/client'));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
